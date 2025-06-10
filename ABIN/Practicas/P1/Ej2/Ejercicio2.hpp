95f36b23b77f410146a36746ca47c29b0066a72c
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-10 17:20:23 +0200

    Ej3P1

---
+++ ABIN/Practicas/P1/Ej2/Ejercicio2.hpp
@@ -0,0 +1,22 @@
+#ifndef _EJERCICIO_2_P1_HPP_
+#define _EJERCICIO_2_P1_HPP_
+
+#include "../../../Abin.hpp"
+#include <algorithm>
+
+/*Implementa un subprograma que calcule la altura de un árbol binario.  */
+
+template <typename T>
+int altura_abin_rec(const Abin<T>& A,typename Abin<T>::nodo n)
+{
+    if (n == Abin<T>::NODO_NULO) return -1;
+    else return 1 + std::max(altura_abin_rec(A, A.hijoIzqdo(n)),altura_abin_rec(A, A.hijoDrcho(n)));
+}
+
+template <typename T>
+int altura_abin(const Abin<T>& A)
+{
+    return altura_abin_rec(A, A.raiz());
+}
+
+#endif
---
+++ ABIN/Practicas/P1/Ej3/Ejercicio3.hpp
@@ -0,0 +1,21 @@
+#ifndef _EJERCICIO_3_P1_HPP_
+#define _EJERCICIO_3_P1_HPP_
+
+#include "../../../Abin.hpp"
+#include <cstddef>
+
+/* Implementa un subprograma que, dados un árbol binario y un nodo del mismo, determine
+ * la profundidad de este nodo en dicho árbol.  */
+
+template<typename T>
+int profundidad_Abin(const Abin<T>& A,typename Abin<T>::nodo n)
+{
+    int prof = 0;
+    while (A.padre(n) != Abin<T>::NODO_NULO) {
+        ++prof;
+        n = A.padre(n);
+    }
+    return prof;
+}
+
+#endif
