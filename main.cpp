21105029586607c23a8d3f37889771ee5f7fc435
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-10 16:55:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include "ABIN/Abin.hpp"
 #include "ABIN/abin_E-S.h"
+#include "ABIN/Practicas/P1/Ej1/Ejercicio1.hpp"
 #include <cstdlib>
 #include <iostream>
 
 int main() {
     Abin<char> A;
     rellenarAbin(A,'#');
-    std::ofstream archivo("archivo.txt");
-    imprimirAbin(archivo,A,'#');
-    Abin<char> B(A);
-    imprimirAbin(std::cout,B,'#');
+    imprimirAbin(std::cout,A,'#');
+    std::cout << num_nodos(A) << " de nodos." << std::endl;
     exit(EXIT_SUCCESS);
 }
