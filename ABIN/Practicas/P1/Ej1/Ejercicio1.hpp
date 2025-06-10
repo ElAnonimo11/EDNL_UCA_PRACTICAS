64f87749e0041c4356cd9e700d99cf81ba7b355c
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-10 16:55:56 +0200

    p1ej1

---
+++ ABIN/Practicas/P1/Ej1/Ejercicio1.hpp
@@ -0,0 +1,21 @@
+#ifndef _EJERCICIO_1_P1_HPP_
+#define _EJERCICIO_1_P1_HPP_
+
+#include "../../../Abin.hpp"
+#include <cstddef>
+
+/*Implementa un subprograma que calcule el número de nodos de un árbol binario */
+template<typename T>
+size_t num_nodos_rec(const Abin<T>& A,typename Abin<T>::nodo n)
+{
+    if(n == Abin<T>::NODO_NULO) return 0;
+    else return 1 + num_nodos_rec(A,A.hijoIzqdo(n)) + num_nodos_rec(A,A.hijoDrcho(n));
+}
+
+template <typename T>
+size_t num_nodos(const Abin<T>& A)
+{
+    return num_nodos_rec(A, A.raiz());
+}
+
+#endif
