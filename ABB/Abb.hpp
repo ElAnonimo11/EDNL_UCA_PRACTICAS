828d4a75b8c968f8e86618b4b84ae6d63969b8b2
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-14 17:12:56 +0200

    Update Abb.hpp

--- ABB/Abb.hpp
+++ ABB/Abb.hpp
@@ -47,5 +47,55 @@ const Abb<T>& Abb<T>::buscar(const T& e) const
     else return *this;
 }
 
+template <typename T>
+void Abb<T>::insertar(const T& e)
+{
+    if (vacio()) r = new arbol{e};
+    else if (e < r->elto) r->izq.insertar(e);
+    else if (r->elto < e) r->der.insertar(e);
+}
+
+template <typename T>void Abb<T>::eliminar(const T& e)
+{
+    if (!vacio()) {
+        if (e < r->elto) r->izq.eliminar(e);
+        else if (r->elto < e) r->der.eliminar(e);
+        else { // Eliminar la raíz.
+            arbol* a; // Sustituto de la raíz.
+            // arbol hoja
+            if (!r->izq.r && !r->der.r)
+                a = nullptr;
+            // Arbol con hijo izquierdo
+            else if (!r->der.r) {
+                a = r->izq.r;
+                r->izq.r = nullptr;
+            }
+            // Arbol con hijo derecho
+            else if (!r->izq.r) {
+                a = r->der.r;
+                r->der.r = nullptr;
+            }
+            // Arbol con dos hijos
+            else {
+                // Buscar el mínimo del subárbol drcho. de la raíz.
+                Abb* pMin = &r->der;
+                while (pMin->r->izq.r != nullptr) pMin = &pMin->r->izq;
+                // Extraer el mínimo y sustituirlo por su hijo drcho.
+                a = pMin->r;
+                pMin->r = a->der.r;
+                // Transferir al mínimo los subárboles de la raíz.
+                a->izq.r = r->izq.r;
+                a->der.r = r->der.r;
+                r->izq.r = nullptr;
+                r->der.r = nullptr;
+            }
+            // Destruir la raíz.
+            delete r;
+            r = a;
+        } // Eliminar la raíz.
+    }
+}
+
+
 
 #endif
