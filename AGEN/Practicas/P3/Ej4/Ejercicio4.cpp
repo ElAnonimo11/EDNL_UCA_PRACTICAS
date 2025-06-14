10bec27ffb87660a999419deb2f107e40397005c
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-14 13:15:17 +0200

    adsa

--- AGEN/Practicas/P3/Ej4/Ejercicio4.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef _EJERCICIO_4_P3_HPP_
-#define _EJERCICIO_4_P3_HPP_
-
-#include "../../../Agen.hpp"
-
-
-
-#endif
---
+++ AGEN/Practicas/P3/Ej4/Ejercicio4.hpp
@@ -0,0 +1,16 @@
+#ifndef _EJERCICIO_4_P3_HPP_
+#define _EJERCICIO_4_P3_HPP_
+
+#include "../../../Agen.hpp"
+
+/*
+ * Dado un árbol general de enteros A y un entero x, implementa un subprograma que realice
+ * la poda de A a partir de x. Se asume que no hay elementos repetidos en A.
+ */
+
+void podaAgen(Agen<int>& A);
+void podaAgen_rec(Agen<int>& A,Agen<int>::nodo n);
+
+void podar_Agen(Agen<int>& A,Agen<int>::nodo n);
+
+#endif
